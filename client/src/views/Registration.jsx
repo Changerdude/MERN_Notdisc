8dd6f9e7ef2e27e2c24ace11dc92ce512d7716fe
Kurt Jeffries <elrordoomhammer@gmail.com>
2023-03-02 13:34:29 -0500

    added some validation to reg

--- client/src/views/Registration.jsx
+++ client/src/views/Registration.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import FormRange from 'react-bootstrap/esm/FormRange';
 
 const Registration = () => {
   const { setAuth } = useAuth();
@@ -13,7 +14,9 @@ const Registration = () => {
   const [pw, setPw] = useState('');
   const [confirmPw, setConfirmPw] = useState('');
   const [pwMatch, setPwMatch] = useState(false);
-  const { errs, setErrs } = useState([]);
+  const [error,setErrors] =useState(false);
+
+  const EMAIL_REGEX = new RegExp('^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$');
 
   const nav = useNavigate();
 
@@ -24,44 +27,50 @@ const Registration = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (pwMatch) {
+    if (pwMatch && EMAIL_REGEX.test(email)) {
       axios.post(`http://localhost:8000/api/users/create`, {
         email,
         username,
         pw
-      },{withCredentials:true})
+      }, { withCredentials: true })
         .then((res) => {
-          setAuth({username: res?.data?.username , accessToken: res?.data?.accessToken});
+          setAuth({ username: res?.data?.username, accessToken: res?.data?.accessToken });
           nav('/main')
         })
         .catch((err) => {
-          console.log(err)
+          setErrors(err.responce?.data?.error?.errors)
         })
-    } else {
-      alert('Passwords need to match');
-      //This will move to validations below
     }
   }
 
   return (
     <>
       <Container>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} className='needs-validation' noValidate>
           <Form.Group>
             <Form.Label>Email:</Form.Label>
-            <Form.Control type='text' onChange={(e) => setEmail(e.target.value)} value={email} required/>
+            <Form.Control type='text' onChange={(e) => setEmail(e.target.value)} value={email} required isInvalid={!EMAIL_REGEX.test(email)}/>
+            <Form.Control.Feedback type='invalid'>
+              Must be a valid email
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Username(This is what people will see your name as):</Form.Label>
-            <Form.Control type='text' onChange={(e) => setUsername(e.target.value)} value={username} required/>
+            <Form.Control type='text' onChange={(e) => setUsername(e.target.value)} value={username} required isInvalid={error?.username}/>
+            <Form.Control.Feedback type='invalid'>
+              {error.username?.message}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Password:</Form.Label>
-            <Form.Control type='password' onChange={(e) => setPw(e.target.value)} value={pw} required/>
+            <Form.Control type='password' onChange={(e) => setPw(e.target.value)} value={pw} required />
           </Form.Group>
           <Form.Group>
             <Form.Label>Confirm Password:</Form.Label>
-            <Form.Control type='password' onChange={(e) => setConfirmPw(e.target.value)} value={confirmPw} required/>
+            <Form.Control type='password' onChange={(e) => setConfirmPw(e.target.value)} value={confirmPw} required isInvalid={!pwMatch} />
+            <Form.Control.Feedback type='invalid'>
+              Passwords must match
+            </Form.Control.Feedback>
           </Form.Group>
           <Button type='submit'>Create User</Button>
         </Form>
